080ad593265fd2b997a07d58f74816a3d0b110b5
Ricardo Gouvêa Mendonça <RicardoGM74@users.noreply.github.com>
2017-05-30 13:05:08 -0400

    test

---
+++ assignment_1/test.js
@@ -0,0 +1,24 @@
+console.log('test');
+
+var name = 'Ricardo';
+console.log('Hello ' + name);
+
+var x = 1125;
+var answer = x%2;
+console.log(answer);
+
+var a = true;
+var b = false;
+var c = true;
+console.log(a && b && c);
+
+var answer1 = 10 - 3 * 5/4 + 202 % 3;
+var answer2 =  10 - (3 * (5/4) + (202 % 3));
+console.log(answer1);
+console.log(answer2);
+
+console.log(String(12));
+console.log(parseInt('113'));
+console.log(parseFloat('1.12356'));
+var x = parseInt('Hello World');
+console.log(x); 
