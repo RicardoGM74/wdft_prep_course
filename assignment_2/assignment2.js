1901074cc74075caa4a20ebae92c4c6e11c7ce79
Ricardo Gouvêa Mendonça <RicardoGM74@users.noreply.github.com>
2017-06-01 11:54:49 -0400

    Loops

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,9 +2,17 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
-for(var i=1;i<=100;i++){
-    console.log(i); 
-}
+for(var i=1;i<=100;i++) {
+    if (Number.isInteger(i/3) && Number.isInteger(i/5)) { 
+        console.log(i + " fizzbuzz");
+    } else if (Number.isInteger(i/3)) {
+        console.log(i + " buzz");
+    } else if (Number.isInteger(i/5)) {
+        console.log(i + " fizz");
+    } else {
+        console.log(i);
+    } (i/3)
+    }
 
 /*
 Exercise #2
@@ -23,3 +31,12 @@ for(var x=1;x<=7;x++){
     console.log((txt = txt +"#")); 
 }
 console.log(txt.length)
+
+var chess = "# # # #";
+for(var x=1;x<=8;x++){
+    if (Number.isInteger(x/2)) {
+        console.log(" " + chess);
+    } else {
+        console.log(chess);
+    } 
+}
